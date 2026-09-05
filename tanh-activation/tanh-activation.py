84df4394e6e02776f0dceee3e68ecc655f98aa04
bqa100507-spec <bqa100507@gmail.com>
2026-09-05 16:57:55 +0700

    Solve tanh-activation

--- tanh-activation/tanh-activation.py
+++ tanh-activation/tanh-activation.py
@@ -5,5 +5,5 @@ def tanh(x: list) -> np.ndarray:
     Returns a NumPy array with the same shape as x.
     """
     x = np.array(x)
-    tanh_x = np.tanh(x)
+    tanh_x = (1 - np.exp(-2*x))/(1 + np.exp((-2*x)))
     return tanh_x
